20235cc0fc8189f330d4676294e5d68ddfdc2b4e
Dirk Lemmermann <dlemmermann@gmail.com>
2022-01-21 15:32:41 +0100

    Initial commit

---
+++ src/main/java/module-info.java
@@ -0,0 +1,12 @@
+open module com.dlsc.showcase {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.swing;
+    requires java.logging;
+    requires javafx.web;
+    requires org.kordamp.ikonli.core;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.ikonli.materialdesign;
+    requires fr.brouillard.oss.cssfx;
+    exports com.dlsc.showcase;
+}
